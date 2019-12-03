2ab286760bb9a768483e7ee6c9640a528563aff8
Richard Curbani Alfarth <richard.curbani.alfarth@hotmail.com>
2019-12-03 19:53:33 -0300

    Ajustando o raycasting

--- Assets/RayCasting.cs
+++ Assets/RayCasting.cs
@@ -10,15 +10,25 @@ public class RayCasting : MonoBehaviour
     [SerializeField]private Material highlightMaterial;
     [SerializeField]private Material defaultMaterial;
     public TextMeshPro showText;
+    public TextMeshPro TextOH;
+    public TextMeshPro TextN;
+    public TextMeshPro TextH;
     public ScrollRect scroll;
     private string selectableTag = "Selectable";
-    public GameObject Quad;
+    public GameObject QuadCL;
+    public GameObject QuadH;
+    public GameObject QuadN;
+    public GameObject QuadOH;
+    public Collider other;
 
     private Transform _selection;
     // Start is called before the first frame update
     void Start()
     {
-        Quad.SetActive(true);
+        QuadCL.SetActive(false);
+        QuadH.SetActive(false);
+        QuadOH.SetActive(false);
+        QuadN.SetActive(false);
 
     }
   
@@ -35,38 +45,67 @@ public class RayCasting : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit))
         {
-            var selection = hit.transform;
-            
+            var selection = hit.transform;            
                 Debug.Log("Selectable");
-            if (GameObject.Find("ImageTargetCL").CompareTag("cl"))
-            {
-                showText.text = "O cloro ( grego chlorós, esverdeado ) é um elemento químico , símbolo Cl de número atômico 17 ( 17 prótons e 17 elétrons ) com massa atômica 35,5 u, encontrado em temperatura ambiente no estado gasoso. Gás extremamente tóxico e de odor irritante, foi descoberto em 1774 pelo sueco Carl Wilhelm Scheele.";
-                scroll.enabled = true;
-                Debug.Log(showText);
-                if (selection.CompareTag(selectableTag))
+              Debug.Log(showText);
+                if (selection.CompareTag("cl"))
                 {
+                    QuadCL.SetActive(true);
+                    showText.text = "O cloro ( grego chlorós, esverdeado ) é um elemento químico ," + "\r\n" + " símbolo Cl de número atômico 17 ( 17 prótons e 17 elétrons ) com massa atômica 35,5 u," + "\r\n" + " encontrado em temperatura ambiente no estado gasoso. Gás extremamente tóxico e de odor irritante," + "\r\n" + " foi descoberto em 1774 pelo sueco Carl Wilhelm Scheele.";
                     var selectionRenderer = selection.GetComponent<Renderer>();
                     if (selectionRenderer != null)
                     {
                         selectionRenderer.material = highlightMaterial;
-                        Debug.Log("Teste RayCasting");
+                        Debug.Log("Teste RayCasting CL");
                         Debug.Log(showText);
-                        Quad.SetActive(true);
+                        
+                    }
+                    _selection = selection;
+                }           
+                Debug.Log(TextN);
+                if (selection.CompareTag("Na"))
+                {
+                    QuadN.SetActive(true);
+                    TextN.text = "O cloro ( grego chlorós, esverdeado ) é um elemento químico ," + "\r\n" + " símbolo Cl de número atômico 17 ( 17 prótons e 17 elétrons ) com massa atômica 35,5 u," + "\r\n" + " encontrado em temperatura ambiente no estado gasoso. Gás extremamente tóxico e de odor irritante," + "\r\n" + " foi descoberto em 1774 pelo sueco Carl Wilhelm Scheele.";
+                    var selectionRenderer = selection.GetComponent<Renderer>();
+                    if (selectionRenderer != null)
+                    {
+                        selectionRenderer.material = highlightMaterial;
+                        Debug.Log("Teste RayCasting Na");
+                        Debug.Log(TextN);
+                        
                     }
                     _selection = selection;
                 }
-
-            }else if (GameObject.Find("ImageTargetN").CompareTag("Na"))
-            {
-
-            }else if (GameObject.Find("ImageTargetOH").CompareTag("OH"))
-            {
-
-            }
-            else if (GameObject.Find("ImageTargetH").CompareTag("H"))
-            {
-
-            }
+                if (selection.CompareTag("OH"))
+                {
+                    QuadOH.SetActive(true);
+                    TextOH.text = "Os álcoois (função álcool) são compostos que apresentam como grupo funcional a hidroxila (‒OH)," + "\r\n" + " ligados a carbonos saturados, em sua estrutura. Podem possuir insaturações, cadeias cíclicas e ramificações." + "\r\n" + " Em geral são solúveis em água e até onze carbonos são líquidos.";
+                    var selectionRenderer = selection.GetComponent<Renderer>();
+                    if (selectionRenderer != null)
+                    {
+                        selectionRenderer.material = highlightMaterial;
+                        Debug.Log("Teste RayCasting OH");
+                        Debug.Log(TextOH);
+                        
+                    }
+                    _selection = selection;
+                }            
+                if (selection.CompareTag("H"))
+                    {
+                    QuadH.SetActive(true);
+                    TextH.text = "Do Grego de Hydro Genes que significa formador de água." + "\r\n" + " Descoberto por Henry Cavendish em 1766 em Londres pela reação do ácido vitriólico (H2SO4) com Zn," + "\r\n" + " Fe ou Sn. Chamou-o de 'ar inflamável'. É um gás incolor," + "\r\n" + " mais leve que o ar com o qual forma mistura explosiva. Possui três isótopos," + "\r\n" + " prótio (H), deutério (D) descoberto em 1932 por Urey e tritio (T) sendo os dois primeiros naturais." + "\r\n" + " O deutério existe no hidrogênio natural na proporção de 1 átomo para 6.000 átomos de prótio. " + "\r\n" + "Pode ser obtido pela eletrólise de uma solução ácido sulfúrico ou clorídrico, utilizando eletrodos inertes, " + "\r\n" + "sendo desprendido no cátodo (pólo negativo).";
+                    var selectionRenderer = selection.GetComponent<Renderer>();
+                        if (selectionRenderer != null)
+                        {
+                            selectionRenderer.material = highlightMaterial;
+                            Debug.Log("Teste RayCasting H");
+                            Debug.Log(TextH);
+                        
+                        }
+                        _selection = selection;
+                  }
+            
         }
     }
 }
